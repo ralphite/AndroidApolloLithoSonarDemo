37dbf1f925f7b933048cb7393ac534c1952d28b4
Yadong Wen <yadongwen@gmail.com>
2018-06-27 09:47:01 -0700

    experiment with sonar

--- app/src/main/java/com/example/yadong/androidapollolithosonardemo/DemoApplication.java
+++ app/src/main/java/com/example/yadong/androidapollolithosonardemo/DemoApplication.java
@@ -4,6 +4,12 @@ package com.example.yadong.androidapollolithosonardemo;
 import android.app.Application;
 
 import com.facebook.soloader.SoLoader;
+import com.facebook.sonar.android.AndroidSonarClient;
+import com.facebook.sonar.android.utils.SonarUtils;
+import com.facebook.sonar.core.SonarClient;
+import com.facebook.sonar.plugins.inspector.DescriptorMapping;
+import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
+import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
 
 public class DemoApplication extends Application {
 
@@ -12,5 +18,14 @@ public class DemoApplication extends Application {
         super.onCreate();
 
         SoLoader.init(this, false);
+
+        if (BuildConfig.DEBUG && SonarUtils.shouldEnableSonar(this)) {
+            final SonarClient client = AndroidSonarClient.getInstance(this);
+            client.addPlugin(new NetworkSonarPlugin());
+
+            client.addPlugin(new InspectorSonarPlugin(
+                    getApplicationContext(), DescriptorMapping.withDefaults()));
+            client.start();
+        }
     }
 }
