14d50ce3c50e49fe869b5e4a486e4d792d340612
Yadong Wen <yadongwen@gmail.com>
2018-06-27 15:22:03 -0700

    extend litho

---
+++ app/src/main/java/com/example/yadong/androidapollolithosonardemo/components/ProjectListItemSpec.java
@@ -0,0 +1,24 @@
+package com.example.yadong.androidapollolithosonardemo.components;
+
+import com.facebook.litho.Column;
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.annotations.LayoutSpec;
+import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.Prop;
+import com.facebook.litho.widget.Text;
+
+import static com.facebook.yoga.YogaEdge.ALL;
+
+@LayoutSpec
+public class ProjectListItemSpec {
+
+  @OnCreateLayout
+  static Component onCreateLayout(ComponentContext c, @Prop String projectName) {
+
+    return Column.create(c)
+        .paddingDip(ALL, 16)
+        .child(Text.create(c).text(projectName).textSizeSp(40))
+        .build();
+  }
+}
--- app/src/main/java/com/example/yadong/androidapollolithosonardemo/components/ProjectListSectionSpec.java
+++ app/src/main/java/com/example/yadong/androidapollolithosonardemo/components/ProjectListSectionSpec.java
@@ -1,22 +1,36 @@
 package com.example.yadong.androidapollolithosonardemo.components;
 
+import com.facebook.litho.annotations.FromEvent;
+import com.facebook.litho.annotations.OnEvent;
 import com.facebook.litho.sections.Children;
 import com.facebook.litho.sections.SectionContext;
 import com.facebook.litho.sections.annotations.GroupSectionSpec;
 import com.facebook.litho.sections.annotations.OnCreateChildren;
-import com.facebook.litho.sections.common.SingleComponentSection;
-import com.facebook.litho.widget.Text;
+import com.facebook.litho.sections.common.DataDiffSection;
+import com.facebook.litho.sections.common.RenderEvent;
+import com.facebook.litho.widget.ComponentRenderInfo;
+import com.facebook.litho.widget.RenderInfo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @GroupSectionSpec
 public class ProjectListSectionSpec {
 
+  @OnCreateChildren
+  static Children onCreateChildren(final SectionContext sectionContext) {
+    return Children.create()
+        .child(
+            DataDiffSection.create(sectionContext)
+                .data(new ArrayList<Object>(Arrays.asList("A", "B")))
+                .renderEventHandler(ProjectListSection.onRender(sectionContext)))
+        .build();
+  }
 
-    @OnCreateChildren
-    static Children onCreateChildren(final SectionContext sectionContext) {
-        return Children.create()
-                .child(SingleComponentSection.create(sectionContext).component(
-                        Text.create(sectionContext).text("hello world").textSizeSp(20).build()
-                ).build())
-                .build();
-    }
+  @OnEvent(RenderEvent.class)
+  static RenderInfo onRender(final SectionContext c, @FromEvent String model) {
+    return ComponentRenderInfo.create()
+        .component(ProjectListItem.create(c).projectName(model).build())
+        .build();
+  }
 }
