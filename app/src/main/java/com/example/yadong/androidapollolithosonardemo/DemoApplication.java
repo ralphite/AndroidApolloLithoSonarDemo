f9fb5a13cb30243c2ffbc30836a8e44ea89c659b
Yadong Wen <yadongwen@gmail.com>
2018-06-27 10:51:34 -0700

    disable sonar for build issue

--- app/src/main/java/com/example/yadong/androidapollolithosonardemo/DemoApplication.java
+++ app/src/main/java/com/example/yadong/androidapollolithosonardemo/DemoApplication.java
@@ -4,12 +4,12 @@ package com.example.yadong.androidapollolithosonardemo;
 import android.app.Application;
 
 import com.facebook.soloader.SoLoader;
-import com.facebook.sonar.android.AndroidSonarClient;
-import com.facebook.sonar.android.utils.SonarUtils;
-import com.facebook.sonar.core.SonarClient;
-import com.facebook.sonar.plugins.inspector.DescriptorMapping;
-import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
-import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
+//import com.facebook.sonar.android.AndroidSonarClient;
+//import com.facebook.sonar.android.utils.SonarUtils;
+//import com.facebook.sonar.core.SonarClient;
+//import com.facebook.sonar.plugins.inspector.DescriptorMapping;
+//import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
+//import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
 
 public class DemoApplication extends Application {
 
@@ -19,13 +19,13 @@ public class DemoApplication extends Application {
 
         SoLoader.init(this, false);
 
-        if (BuildConfig.DEBUG && SonarUtils.shouldEnableSonar(this)) {
-            final SonarClient client = AndroidSonarClient.getInstance(this);
-            client.addPlugin(new NetworkSonarPlugin());
-
-            client.addPlugin(new InspectorSonarPlugin(
-                    getApplicationContext(), DescriptorMapping.withDefaults()));
-            client.start();
-        }
+//        if (BuildConfig.DEBUG && SonarUtils.shouldEnableSonar(this)) {
+//            final SonarClient client = AndroidSonarClient.getInstance(this);
+//            client.addPlugin(new NetworkSonarPlugin());
+//
+//            client.addPlugin(new InspectorSonarPlugin(
+//                    getApplicationContext(), DescriptorMapping.withDefaults()));
+//            client.start();
+//        }
     }
 }
