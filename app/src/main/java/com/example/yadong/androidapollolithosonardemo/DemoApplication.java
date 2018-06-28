c9c35e6a3da2b47b45a0dbb94fa23f8e3d7ae085
Yadong Wen <yadongwen@gmail.com>
2018-06-27 23:16:57 -0700

    use stetho for now

--- app/src/main/java/com/example/yadong/androidapollolithosonardemo/DemoApplication.java
+++ app/src/main/java/com/example/yadong/androidapollolithosonardemo/DemoApplication.java
@@ -4,26 +4,30 @@ import android.app.Application;
 
 import com.apollographql.apollo.ApolloClient;
 import com.facebook.soloader.SoLoader;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import okhttp3.OkHttpClient;
 
 public class DemoApplication extends Application {
 
-  private static final String BASE_URL = "http://127.0.0.1:8000/graphql";
-  private ApolloClient apolloClient;
+    private static final String BASE_URL = "http://127.0.0.1:8000/graphql";
+    private ApolloClient apolloClient;
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    SoLoader.init(this, false);
+        SoLoader.init(this, false);
 
-    OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        Stetho.initializeWithDefaults(this);
 
-    apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
-  }
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addNetworkInterceptor(new StethoInterceptor()).build();
 
-  public ApolloClient apolloClient() {
-    return apolloClient;
-  }
+        apolloClient = ApolloClient.builder().serverUrl(BASE_URL).okHttpClient(okHttpClient).build();
+    }
+
+    public ApolloClient apolloClient() {
+        return apolloClient;
+    }
 }
